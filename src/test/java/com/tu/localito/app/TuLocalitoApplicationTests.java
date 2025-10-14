340fa022706ddc237aa9a5b0d90c1415e4f2f42d
Jorge Rodriguez <jarf4321@gmail.com>
2025-10-13 21:08:58 -0600

    First commit

---
+++ src/test/java/com/tu/localito/app/TuLocalitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu.localito.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuLocalitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
