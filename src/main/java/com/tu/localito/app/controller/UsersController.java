429200376c3b8a1c86f223cfa64db460d1d073f4
Aarón De la Rosa <aarondrpqti@gmail.com>
2025-10-20 11:19:35 -0600

    feat usersController bad

--- src/main/java/com/tu/localito/app/controller/UsersController.java
+++ src/main/java/com/tu/localito/app/controller/UsersController.java
@@ -1,18 +1,49 @@
 package com.tu.localito.app.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tu.localito.app.model.Users;
 import com.tu.localito.app.service.UsersService;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/users")
 public class UsersController {
 	private final UsersService userService;
 
 	public UsersController(UsersService userService) {
 		this.userService = userService;
 	}
+	
+	@PostMapping
+	ResponseEntity<Users> createdUser(@RequestBody Users user){
+		Users createdUser = userService.save(user);
+		return ResponseEntity.status(201).body(createdUser);
+	}
+	
+	@GetMapping("/{id}")
+	ResponseEntity<Users> findById(@PathVariable("id") Long id){
+		Users existingUser = userService.findById(id);
+		return ResponseEntity.ok(existingUser);
+	}
+	
+	@GetMapping 
+	ResponseEntity<Iterable<Users>> getAllRoles(){
+		return ResponseEntity.ok(userService.findAll());
+	}
+	
+	@DeleteMapping("/{id}")
+	ResponseEntity<Void> deleteById(@PathVariable("id")Long id){
+		userService.deleteByID(id);
+		return ResponseEntity.noContent().build();		
+	}
+	
 	
 	
 }
