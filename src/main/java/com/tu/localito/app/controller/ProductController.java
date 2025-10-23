135296a6b29b81776c49100e0f443b050e1e050b
Jorge Rodriguez <jarf4321@gmail.com>
2025-10-23 15:43:43 -0600

    Fix: obtener productos localmente

--- src/main/java/com/tu/localito/app/controller/ProductController.java
+++ src/main/java/com/tu/localito/app/controller/ProductController.java
@@ -1,15 +1,21 @@
 package com.tu.localito.app.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.tu.localito.app.model.Product;
 import com.tu.localito.app.service.ProductService;
 
+@CrossOrigin( // ← agrega esto
+		origins = { "http://127.0.0.1:5500", "http://localhost:5500" }, methods = { RequestMethod.GET,
+				RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE,
+				RequestMethod.OPTIONS }, allowedHeaders = { "*" })
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -17,17 +23,17 @@ public class ProductController {
 
 	public ProductController(ProductService productService) {
 		this.productService = productService;
-	} 
-	
-	@GetMapping 
-	ResponseEntity<Iterable<Product>> getAllProduct(){
+	}
+
+	@GetMapping
+	ResponseEntity<Iterable<Product>> getAllProduct() {
 		return ResponseEntity.ok(productService.findAll());
 	}
-	
+
 	@PostMapping
-	ResponseEntity <Product> productCreated (@RequestBody Product product){
+	ResponseEntity<Product> productCreated(@RequestBody Product product) {
 		Product productCreated = productService.save(product);
 		return ResponseEntity.status(201).body(productCreated);
 	}
-	
+
 }
